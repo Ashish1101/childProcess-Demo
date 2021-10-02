cb2de9a82ddc20aa6b92816ae5dde3e1f39c23c9
Ashish Personal <ashishskkumar321@gmail.com>
2021-10-02 21:35:26 +0530

    server and isPrime addeed

---
+++ isPrime.js
@@ -0,0 +1,24 @@
+//this child module is now associated to main process
+//so 
+
+
+process.on('message' , message => {
+    let isPrime = checkIfPrime(message);
+
+    //send the processed data back to main process
+    process.send(isPrime)
+
+    //kill the child process after its work is done
+    process.exit();
+})
+
+
+function checkIfPrime(number){
+    let isPrime = true;
+    for (let i = 2; i < number; i++){
+        if(number % i === 0){
+            isPrime = false;
+        }
+    }
+    return isPrime && number > 1;
+}
